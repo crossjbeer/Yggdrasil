972fc7542fd8497e926aef665e4e03c7696bbc98
Crossland <crossjbeer@gmail.com>
2023-06-21 13:41:27 -0400

    Importing chatter and colorcodes from chatter. made 'query' argument optional. If no query is provided, a query is requested. Added colors to various print output. Now using chatter to facilitate conversation with chat gpt. Passing initial set of messages as a list to chatter, using the 'extra messages' parameter.

--- chat.py
+++ chat.py
@@ -4,11 +4,15 @@ import argparse
 import pinecone 
 import pandas as pd 
 
+from chatter import Colorcodes, Chatter
+
 openai.api_key = os.getenv('OPENAI_AUTH')
+
 # Alternatively just set your own api key (just don't upload to git!)
 pinecone.init(api_key = '27f2b80f-96a2-4c4f-baba-262615e29ac2', environment='asia-southeast1-gcp-free')
 
 INDEX = pinecone.Index('ygg')
+COLORCODE = Colorcodes()
 
 def embeddingToText(path):
     res = pd.read_csv(path, header=0)
@@ -21,29 +25,33 @@ def estimateTokens(s, charPerToken = 4):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-q', '--query', help='Query for Chat GPT', type=str, default=None)
+    parser.add_argument('-q', '--query', help='Query for Chat GPT', type=str, default=None, required=False)
     parser.add_argument('-m', '--model', help='Open AI model to use [gpt3.5-turbo, gpt4]', type=str, default='gpt-4')
     parser.add_argument('-n', '--nvector', help='Number of vectors to query', type=int, default=25)
     parser.add_argument('-p', '--path', type=str, help='Path to directory containing <date> folders with embedding files...')
 
     args = parser.parse_args() 
 
-    print("Converting query into Embedding...")
+    prompt = args.query
+    if(not prompt or not len(prompt)):
+        prompt = input(f"{COLORCODE.blue}Prompt> {COLORCODE.reset}")
+        print()
+
+    print(f"{COLORCODE.red}Converting query into Embedding...{COLORCODE.reset}")
     response = openai.Embedding.create(
-            input=args.query,
+            input=prompt,
             model='text-embedding-ada-002'
     )
-    print("Embedding Complete\n")
-    
     embeddings = response['data'][0]['embedding']
+    print(f"{COLORCODE.orange}Embedding Complete{COLORCODE.reset}\n")
 
-    print("Using Embedding to Query Pinecone for top {} Vectors...".format(args.nvector))
+    print(f"{COLORCODE.red}Using Embedding to Query Pinecone for top {args.nvector} Vectors...{COLORCODE.reset}")
     relatedVectors = INDEX.query(
         top_k=args.nvector,
         vector=embeddings ,
         include_values=True
     )
-    print("Pinecone Query Complete\n")
+    print(f"{COLORCODE.orange}Pinecone Query Complete{COLORCODE.reset}\n")
 
     relatedID = [i['id'] for i in relatedVectors['matches']]
 
@@ -77,11 +85,13 @@ def main():
         sessionToText[session] = ""
 
         if(len(sessionToID[session]) <= 1):
-            sessionToText[session] = sessionToIndexToText[session][sessionToID[session][0][0]]
+            #print("SESSION TO ID: {}".format(sessionToID[session]))
+            sessionToText[session] = sessionToIndexToText[session][sessionToID[session][0]]
+            #print("STT:")
+            #print(sessionToText[session])
 
         else:
             for i in range(len(sessionToID[session])-1):
-                #grab the current and next session. Look for overlap. Remove if present.
                 currentInd = sorted(sessionToID[session])[i]
                 nextInd    = sorted(sessionToID[session])[i+1]
 
@@ -102,7 +112,8 @@ def main():
     allText = "\n".join(sessionToText[i] for i in sessionToText)
 
 
-    print("Querying OpenAI using {} | {} Tokens...".format(args.model, estimateTokens(allText)))
+    #print("Querying OpenAI using {} | {} Tokens...".format(args.model, estimateTokens(allText)))
+    """
     response = openai.ChatCompletion.create(
         model=args.model,
         messages = [
@@ -113,9 +124,20 @@ def main():
             {'role':'user', 'content':allText}
         ]
     )
+    """
+    messages = [
+            {'role':'system', 'content':'You are a Dungeons and Dragons AI'},
+            {'role':'user', 'content':'Below is a query provided by the user. You should use the information provided further down to answer this query as well as possible.'},
+            #{'role':'user', 'content': args.query},]
+            {'role':'user', 'content':prompt},
+            {'role':'user', 'content': 'Below is the context provided. You will see a text transcript of a dungeons and dragons campaign. Each line is labeled with a speaker. Here are the speaker codes, names, and character / role:\n1) cro: Crossland (DM)\n2) let: Leticia (Russet Crow)\n3) ric: Richard (Likkvorn)\n4) sim: Simon (Lief) 5) ben: Ben (Oskar) 6) kacie: Kacie (Isra). Please read through what was said, and help with the query as best you can.'},
+        ]
+    
+    chat = Chatter(args.model)
+    chat.chat(allText, True, messages)
 
-    print("\n\n\n~~~ Chat GPT Says ~~~")
-    print(response['choices'][0]['message']['content'])
+    #print("\n\n\n~~~ Chat GPT Says ~~~")
+    #print(response['choices'][0]['message']['content'])
     
 
 if (__name__ == "__main__"):
