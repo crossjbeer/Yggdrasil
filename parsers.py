0fd5d520cb000a0909292b1f31603b3ff9678c09
Crossland Beer <crossjbeer@gmail.com>
2024-01-20 12:22:21 -0500

    Changing some default args

--- parsers.py
+++ parsers.py
@@ -24,7 +24,7 @@ def parser_sql(parser):
 
 def parser_gpt(parser):
     parser.add_argument('-q', '--query', help='Query for Chat GPT', type=str, default=None, required=False)
-    parser.add_argument('-m', '--model', help='Open AI model to use [gpt3.5-turbo, gpt4] (Default: gpt-3.5-turbo)', type=str, default='gpt-3.5-turbo')
+    parser.add_argument('-m', '--model', help='Open AI model to use [gpt3.5-turbo, gpt4] (Default: gpt-3.5-turbo-1106)', type=str, default='gpt-3.5-turbo-1106')
     parser.add_argument('-n', '--nvector', help='Number of vectors to query', type=int, default=5)
     parser.add_argument('-eb', '--embedder', help='Open AI Embedding Model (Default: text-embedding-ada-002)', default='text-embedding-ada-002', type=str)
 
